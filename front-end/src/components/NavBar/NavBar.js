4fa244faaf1d16350b3f33096e4e9593e27a28ad
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-20 13:57:24 -0400

    added modal

--- front-end/src/components/NavBar/NavBar.js
+++ front-end/src/components/NavBar/NavBar.js
@@ -4,22 +4,48 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
 
+    state = {
+        showModal: false
+    }
+
+    signUp = (e) => {
+        this.setState({
+            showModal: true
+        })
+    }
+
+    closeModal = (e) => {
+        this.setState({
+            showModal: false
+        })
+    }
+
     render() { 
         return (     
             <div className='container-fluid nav'>
                 <div className='row'>
                     <nav className='transparent'>
                         <div className='nav-wrapper'>
-                            <Link to='/' className='left'>AirBnB</Link>
+                            <Link to='/' className='left site-title'>AirBnB</Link>
                             <ul id='nav-mobile' className='right'>
                                 <li><Link to='/host/homes'>Host a Home</Link></li>
                                 <li><Link to='/host/experience'>Host an Experience</Link></li>
                                 <li><Link to='/help'>Help</Link></li>
-                                <li><Link to='/sign-up'>Sign Up</Link></li>
-                                <li><Link to='/log-in'>Log In</Link></li>
+                                <li className='nav-non-link' onClick={this.signUp}>
+                                    {/* <Link to='/sign-up'>Sign Up</Link> */}
+                                    Sign Up
+                                </li>
+                                <li className='nav-non-link' onClick={this.signUp}>
+                                    {/* <Link to='/log-in'>Log In</Link> */}
+                                    Log In
+                                </li>
                             </ul>
                         </div>
                     </nav>
+                </div>
+                <div className='login-modal' style={this.state.showModal ?  {'display': 'block'}: {}}>
+                    <button className='close-modal' onClick={this.closeModal}>x</button>
+                    <h1>Modal</h1>
                 </div>
             </div>
         );
