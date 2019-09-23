3c11cccea67c890c9fab511a6b98eeb7ae73b06a
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-23 11:49:30 -0400

    Added signUpAction

---
+++ front-end/src/actions/signUpAction.js
@@ -0,0 +1,6 @@
+export default (data) => {
+    return( {
+        type: 'signUp',
+        payload: data
+    })
+}
--- front-end/src/components/NavBar/SignUp.js
+++ front-end/src/components/NavBar/SignUp.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import signUpAction from '../../actions/signUpAction'
 
 class SignUp extends Component {
 
@@ -6,7 +9,7 @@ class SignUp extends Component {
         email: '',
         first: '',
         last: '',
-        password: ''
+        pass: ''
     }
 
     changeEmail = (e) => {
@@ -30,20 +33,32 @@ class SignUp extends Component {
         })
     }
 
+    submitSignup = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
 
     render() {
         return (
             <div className="register-form">
-                <input onChange={() => this.changeEmail} className="email-signup" placeholder="Email address" />
-                <input onChange={() => this.changeFirst} className="first-signup" placeholder="First name" />
-                <input onChange={() => this.changeLast} className="last-signup" placeholder="Last name" />
-                <input onChange={() => this.changePass} className="password-signup" type='password' placeholder="Password" />
-                <button className="sign-up-button">Sign up</button>
-                <div className="border-rule"></div>
-                <div className="login-text align-left">Already have an Airbnb account? <span onClick={() => {this.props.changeModalContent('login')}}>Log In</span></div>
+                <form onSubmit={this.submitSignup}>
+                    <input onChange={this.changeEmail} className="email-signup" placeholder="Email address" />
+                    <input onChange={this.changeFirst} className="first-signup" placeholder="First name" />
+                    <input onChange={this.changeLast} className="last-signup" placeholder="Last name" />
+                    <input onChange={this.changePass} className="password-signup" type='password' placeholder="Password" />
+                    <button className="sign-up-button waves-effect waves-light">Sign up</button>
+                    <div className="border-rule"></div>
+                    <div className="login-text align-left">Already have an Airbnb account? <span onClick={() => {this.props.changeModalContent('login')}}>Log In</span></div>
+                </form>
             </div> 
         )
     }
 }
 
-export default SignUp;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators( {
+        signUpAction
+    }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SignUp);
